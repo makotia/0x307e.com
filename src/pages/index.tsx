86df35575c107e2bc509c4526a8b115671ed2fa2
makotia <3mdevmail@gmail.com>
2021-04-28 22:38:04 +0900

    Jobs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,18 @@
 import { h, FunctionalComponent } from "preact"
 
 import Container from "@/components/Container"
+import Jobs from "@/components/Jobs"
 import SNS from "@/components/SNS"
 import Spacer from "@/components/Spacer"
 
 const Home: FunctionalComponent = () => {
   return (
-    <div>
-      <Container>
-        <SNS />
-        <Spacer height={64} />
-      </Container>
-    </div>
+    <Container>
+      <Jobs />
+      <Spacer height={64} />
+      <SNS />
+      <Spacer height={64} />
+    </Container>
   )
 }
 
