697c3ddcbd828ace23890b8916c5592d73f08fed
makotia <3mdevmail@gmail.com>
2021-04-29 14:34:38 +0900

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 /** @jsx h */
 import { h, FunctionalComponent } from "preact"
+
+import * as gtag from "@/lib/gtag"
+import { useRouter } from "next/router"
+import { useEffect } from "preact/hooks"
 import "normalize.css"
 import "@/styles/global.css"
 
@@ -9,6 +13,16 @@ type Props = {
 }
 
 const App: FunctionalComponent<Props> = ({ Component, pageProps }: Props) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />
 }
 
